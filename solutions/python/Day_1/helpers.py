33f3ff77ebb820c19d85c334fda21371811850e8
Mukilan Krishnakumar <mukilan.git@gmail.com>
2026-01-01 15:53:03 +0530

    [PYTHON] Day 1: Secret Entrance

---
+++ solutions/python/Day_1/helpers.py
@@ -0,0 +1,1 @@
+# Should modularize the helpers into this common file and use these
---
+++ solutions/python/Day_1/part_1_solution.py
@@ -0,0 +1,46 @@
+file_path = "./puzzle.txt"
+
+instructions = [x.strip("\n") for x in open(file_path).readlines()]
+
+current_position = 50
+
+
+def move_left(current_position, steps):
+    actual_steps = abs(steps) % 100
+    landing_position = current_position - actual_steps
+    if landing_position < 0:
+        landing_position = 100 - abs(landing_position)
+    return landing_position
+
+
+def move_right(current_position, steps):
+    actual_steps = abs(steps) % 100
+    landing_position = current_position + steps
+    if landing_position > 99:
+        difference = landing_position % 100
+        landing_position = difference
+    return landing_position
+
+
+def parse_instruction(instruction):
+    movement = instruction[0]
+    steps = int(instruction[1:])
+    return movement, steps
+
+
+key = 0
+
+for instruction in instructions:
+    movement, steps = parse_instruction(instruction)
+    if movement == "L":
+        landing_position = move_left(current_position, steps)
+    else:
+        landing_position = move_right(current_position, steps)
+
+    current_position = landing_position
+    print(f"The Dial is rotated {instruction} to point to {landing_position}")
+    if landing_position == 0:
+        key += 1
+
+
+print("Key: ", key)
