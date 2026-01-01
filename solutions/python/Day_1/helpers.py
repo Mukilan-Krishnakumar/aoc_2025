b2639b7306dea8a043b2342a65c106d780383e4d
Mukilan Krishnakumar <mukilan.git@gmail.com>
2026-01-01 16:11:35 +0530

    [PYTHON] Day 1: Secret Entrance - Refactored

--- solutions/python/Day_1/helpers.py
+++ solutions/python/Day_1/helpers.py
@@ -1,1 +1,33 @@
-# Should modularize the helpers into this common file and use these
+WINDOW_SIZE = 100
+
+
+def get_instructions(file_path):
+    instructions = [x.strip("\n") for x in open(file_path).readlines()]
+    return instructions
+
+
+def parse_instruction(instruction):
+    movement = instruction[0]
+    steps = int(instruction[1:])
+    return movement, steps
+
+
+def move_left(current_position, steps):
+    actual_steps = abs(steps) % WINDOW_SIZE
+    landing_position = current_position - actual_steps
+    if landing_position < 0:
+        landing_position = WINDOW_SIZE - abs(landing_position)
+    return landing_position
+
+
+def move_right(current_position, steps):
+    actual_steps = abs(steps) % WINDOW_SIZE
+    landing_position = current_position + steps
+    if landing_position > WINDOW_SIZE - 1:
+        difference = landing_position % WINDOW_SIZE
+        landing_position = difference
+    return landing_position
+
+
+def get_formatted_output(instruction, landing_position):
+    return f"The Dial is rotated {instruction} to point to {landing_position}"
